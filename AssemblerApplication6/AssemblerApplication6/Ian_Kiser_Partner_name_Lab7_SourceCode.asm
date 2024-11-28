9b1845a5480174a7a82d01a8d669794b7afd637a
EnAtOSU <kiseri@oregonstate.edu>
2024-11-27 23:29:11 -0800

    added initial files for project

---
+++ AssemblerApplication6/AssemblerApplication6/Ian_Kiser_Partner_name_Lab7_SourceCode.asm
@@ -0,0 +1,92 @@
+
+;***********************************************************
+;*
+;*	This is the TRANSMIT skeleton file for Lab 7 of ECE 375
+;*
+;*  	Rock Paper Scissors
+;* 	Requirement:
+;* 	1. USART1 communication
+;* 	2. Timer/counter1 Normal mode to create a 1.5-sec delay
+;***********************************************************
+;*
+;*	 Author: Ian Kiser, Evan Something
+;*	   Date: 12/6/2024
+;*
+;***********************************************************
+
+.include "m32U4def.inc"         ; Include definition file
+
+;***********************************************************
+;*  Internal Register Definitions and Constants
+;***********************************************************
+.def    mpr = r16               ; Multi-Purpose Register
+
+; Use this signal code between two boards for their game ready
+.equ    SendReady = 0b11111111
+
+;***********************************************************
+;*  Start of Code Segment
+;***********************************************************
+.cseg                           ; Beginning of code segment
+
+;***********************************************************
+;*  Interrupt Vectors
+;***********************************************************
+.org    $0000                   ; Beginning of IVs
+	    rjmp    INIT            	; Reset interrupt
+
+
+.org    $0056                   ; End of Interrupt Vectors
+
+;***********************************************************
+;*  Program Initialization
+;***********************************************************
+INIT:
+	;Stack Pointer (VERY IMPORTANT!!!!)
+	ldi mpr, low(RAMEND) ;retrieve ramend low from program memory
+	out SPL, mpr ;load ramend low into stack pointer low via mpr, out is needed as SP(stack pointer) is in io mem
+	ldi mpr, high(RAMEND);retrieve ramend high from program memory
+	out SPH, mpr ;load ramend high into stack pointer low via mpr
+	;I/O Ports
+
+	;USART1
+		;Set baudrate at 2400bps
+		;Enable receiver and transmitter
+		;Set frame format: 8 data bits, 2 stop bits
+
+	;TIMER/COUNTER1
+		;Set Normal mode
+
+	;Other
+
+
+;***********************************************************
+;*  Main Program
+;***********************************************************
+MAIN:
+
+	;TODO: ???
+
+		rjmp	MAIN
+
+;***********************************************************
+;*	Functions and Subroutines
+;***********************************************************
+
+;***********************************************************
+;*	Stored Program Data
+;***********************************************************
+
+;-----------------------------------------------------------
+; An example of storing a string. Note the labels before and
+; after the .DB directive; these can help to access the data
+;-----------------------------------------------------------
+STRING_START:
+    .DB		"Welcome!"		; Declaring data in ProgMem
+STRING_END:
+
+;***********************************************************
+;*	Additional Program Includes
+;***********************************************************
+.include "LCDDriver.asm"		; Include the LCD Driver
+
